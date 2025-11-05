d52796fb55d1c9c00315bad8958ff0e06aafef02
João Tonini <cazuza@badenpowell.richmond.edu>
2025-11-05 16:07:16 -0500

    Ensure sync_nodes.sh exits with success code

--- sync_nodes.sh
+++ sync_nodes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 nodes="node01 node02 node03 node51 node52 node53"
 
@@ -6,5 +6,8 @@ for node in $nodes; do
     echo "refreshing script"
     scp sync_all.sh root@$node:.
     echo "creating user accounts on $node"
-    ssh $node "./sync_all 10.0.0.254:/etc"
+    ssh root@$node "./sync_all.sh 10.0.0.254:/etc"
 done
+
+# Explicitly exit with success
+exit 0
